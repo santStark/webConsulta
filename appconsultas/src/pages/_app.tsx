64451207fa3ce1f8b4dae6382ae7e6459b74a72f
Guilherme Santiago <71676627+santStark@users.noreply.github.com>
2021-05-25 09:50:30 -0300

    Add files via upload
    upp

--- appconsultas/src/pages/_app.tsx
+++ appconsultas/src/pages/_app.tsx
@@ -1,9 +1,20 @@
 import '../styles/globals.css';
-import { Menu } from "../componentes/Menu/menu"
+import styles from './styleIndex.module.css';
+import stylesPaciente from './pacientes/styles.module.css';
+import stylesMedico from './medicos/styles.module.css';
+import stylesListagem from './listagem/styles.module.css';
+import stylesConsultas from './consultas/styles.module.css';
+import { Menu } from "../componentes/Menu/menu";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+    <span className={
+      styles.containerIndex +' '+
+      stylesPaciente.containerPacientes+' '+
+      stylesMedico.containerMedicos+' '+
+      stylesListagem.containerListagem+' '+
+      stylesConsultas.containerConsulta } style={{display:'none'}}></span>
     <div className='divBack'></div>
     <div className='container'>
       <Menu />
--- appconsultas/src/pages/index.tsx
+++ appconsultas/src/pages/index.tsx
@@ -1,40 +1,56 @@
-import { GetStaticProps } from 'next';
-import {api} from '../services/api';
+import { api } from '../services/api';
 import styles from './styleIndex.module.css';
 import Link from 'next/link';
-import {format, parseISO} from 'date-fns';
-import ptBR from 'date-fns/locale/pt-BR';
+import { useEffect, useState } from 'react';
 
-interface IConsultas{
-  id : number,
+interface IConsultas {
+  id: number,
   paciente: string,
   medico: string,
   dia: string,
   hora: string,
 }
 
-interface IProps{
-  consultas:IConsultas[]
+interface IProps {
+  consultas: IConsultas[]
 }
 
-function seachConsulta(e){
+export default function Home(props: IProps) {
 
-  const v = e.target.value.toUpperCase();
-  const el = document.querySelector('.'+styles.containerConsultas);
-  
-  if(!el) return;
+  const [consultas, setConsultas] = useState([]);
 
-  Array.from(el.querySelectorAll('.'+ styles.cardConsulta)).forEach((item:HTMLElement) =>{
+  async function getConsultas() {
+
+    const { data } = await api.get('consultas');
+    if (data.length === 0) return;
+
+    setConsultas(data);
+
+  }
+
+  useEffect(() => {
+
+    getConsultas();
+
+  }, []);
+
+  function seachConsulta(e) {
+
+    const v = e.target.value.toUpperCase();
+    const el = document.querySelector('.' + styles.containerConsultas);
+
+    if (!el) return;
+
+    Array.from(el.querySelectorAll('.' + styles.cardConsulta)).forEach((item: HTMLElement) => {
 
       const p = item.getAttribute('data-card').toUpperCase();
       item.style.display = '';
-      if(!p || p.indexOf(v) < 0 ) item.style.display = 'none';
+      if (!p || p.indexOf(v) < 0) item.style.display = 'none';
 
 
-  })
-}
+    })
+  }
 
-export default function Home(props:IProps) {
   return (
     <div className={styles.containerIndex}>
       <h3 className="fas fa-calendar-alt fa-3x"> CONSULTAS</h3>
@@ -44,40 +60,30 @@ export default function Home(props:IProps) {
           <a className="fas fa-calendar-plus fa-2x" title="Nova Consulta"></a>
         </Link>
       </div>
-      <div className={styles.containerConsultas}> 
+      <div className={styles.containerConsultas}>
         {
-          props.consultas.map((item) =>{
+          consultas.map((item) => {
 
-            return(
-                <div key={item.id} data-card={item.dia+item.medico} className={styles.cardConsulta}>
-                  <Link href={ '/consultas/'+item.id }>
-                    <a className="fas fa-search"></a>
-                  </Link>
-                  <span className="fas fa-calendar-alt"> {' ' +item.dia }</span>
-                  <span className="fas fa-clock"> {' ' + item.hora}</span>
-                  <span className="fas fa-user-nurse"> {' ' + item.medico}</span>
-                  <span className="fa fa-user"> {' ' + item.paciente}</span>
-                  
-                    
-                </div>
+            return (
+              <div key={item.id} data-card={item.dia + item.medico} className={styles.cardConsulta}>
+                <Link href={'/consultas/' + item.id}>
+                  <a className="fas fa-search"></a>
+                </Link>
+                <span className="fas fa-calendar-alt"> {' ' + item.dia}</span>
+                <span className="fas fa-clock"> {' ' + item.hora}</span>
+                <span className="fas fa-user-nurse"> {' ' + item.medico}</span>
+                <span className="fa fa-user"> {' ' + item.paciente}</span>
+
+
+              </div>
             );
 
           })
         }
       </div>
-      
+
     </div>
   )
 }
 
-export const getStaticProps: GetStaticProps = async() => {
-  const {data} = await api.get('consultas');
 
-  return{
-    props:{
-      consultas: data,
-
-    },
-    revalidate: 60 * 60 * 8,
-  }
-}
